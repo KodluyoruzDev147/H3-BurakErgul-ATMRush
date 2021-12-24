3a78e7a81f31c7a4a75846dc9eb02392f72935c9
Zeren Tolga Kaçar <tolga@happygamecompany.com>
2021-12-25 00:51:57 +0300

    Comments

--- Assets/[Game]/Scripts/GameManager.cs
+++ Assets/[Game]/Scripts/GameManager.cs
@@ -1,8 +1,14 @@
-using System;
+﻿using System;
 using _Game_.Scripts.Helpers;
 using _Game_.Scripts.Input;
 using UnityEngine;
 
+/* ZTK was here
+ * Namespace kullanımı genelde oyun içerisinde kullanılan ayrık sistemlerin iç işleyişini oyun kodlarından ayırmak için olur.
+ * Ya da API yazılıyorsa aynı isimde class olabilme ihtimalini ortadan kaldırmak için yine bir namespace içinde yazılabilir.
+ * Oyun kodlarını bu denli derinleşen namespace ler içine gizlemek Intellisense kullanımının amacını baltalar.
+ * Projenin kod okunabilirliğini ağır oranda düşürür ve her yeni bir kod içine using yazmaya uğraştırır.
+ */
 namespace _Game_.Scripts
 {
   public class GameManager : Singleton<GameManager>
